a8c706996230e554e5702919f3aa7967a3f1548d
Phil Spelman <phil_spelman@hotmail.com>
2021-07-06 11:55:42 -0400

    - got starter code for async testing demo https://www.youtube.com/watch?v=4Fl5GH4eYZ8

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
-const {checkAndGenerate} = require("./util");
-const { generateText, createElement, validateInput } = require('./util');
+// const {checkAndGenerate} = require("./util");
+// const { generateText, createElement, validateInput } = require('./util');
+//
+// const initApp = () => {
+//   // Initializes the app, registers the button click listener
+//   const newUserButton = document.querySelector('#btnAddUser');
+//   newUserButton.addEventListener('click', addUser);
+// };
+//
+// const addUser = () => {
+//   // Fetches the user input, creates a new HTML element based on it
+//   // and appends the element to the DOM
+//   const newUserNameInput = document.querySelector('input#name');
+//   const newUserAgeInput = document.querySelector('input#age');
+//
+//   const userList = document.querySelector('.user-list');
+// 	// const outputText = generateText(
+// 	//   newUserNameInput.value,
+// 	//   newUserAgeInput.value
+// 	// );
+//   const outputText = checkAndGenerate(
+//     newUserNameInput.value,
+//     newUserAgeInput.value
+//   );
+//   const element = createElement('li', outputText, 'user-item');
+//   userList.appendChild(element);
+// };
+//
+// // Start the app!
+// initApp();
 
-const initApp = () => {
-  // Initializes the app, registers the button click listener
-  const newUserButton = document.querySelector('#btnAddUser');
-  newUserButton.addEventListener('click', addUser);
+
+// async testing intro section
+const { fetchData } = require('./http');
+
+const button = document.querySelector('button');
+
+const loadTitle = () => {
+  return fetchData().then(extractedData => {
+    const title = extractedData.title;
+    const transformedTitle = title.toUpperCase();
+    return transformedTitle;
+  });
 };
 
-const addUser = () => {
-  // Fetches the user input, creates a new HTML element based on it
-  // and appends the element to the DOM
-  const newUserNameInput = document.querySelector('input#name');
-  const newUserAgeInput = document.querySelector('input#age');
-
-  const userList = document.querySelector('.user-list');
-	// const outputText = generateText(
-	//   newUserNameInput.value,
-	//   newUserAgeInput.value
-	// );
-  const outputText = checkAndGenerate(
-    newUserNameInput.value,
-    newUserAgeInput.value
-  );
-  const element = createElement('li', outputText, 'user-item');
-  userList.appendChild(element);
+const printTitle = () => {
+  loadTitle().then(title => {
+    console.log(title);
+  });
 };
 
-// Start the app!
-initApp();
+button.addEventListener('click', printTitle);
+
+exports.printTitle = printTitle;
--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const {checkAndGenerate} = require("./util");
 const {generateText} = require('./util');
 
@@ -17,7 +18,31 @@ test('should output data-less text', () => {
 
 
 // Integration tests
-test('should generate a valid text output', ()=>{
+test('should generate a valid text output', () => {
 	const text = checkAndGenerate('Phil', 35)
 	expect(text).toBe('Phil (35 years old)')
 })
+
+
+// End-to-End tests
+test('should click around', async () => {
+	const browser = await puppeteer.launch({
+		headless: false,
+		slowMo: 80,
+		args: ['--window-size=1920,1080']
+	})
+	// await the browser to create a new page
+	const page = await browser.newPage();
+	await page.goto(
+		'file:///Users/phil/coding/jest/js-testing-introduction/index.html'
+	)
+	await page.click('input#name');
+	await page.type('input#name', 'Phil');
+	await page.click('input#age');
+	await page.type('input#age', '35');
+	await page.click('#btnAddUser');
+
+	const finalText = await page.$eval('.user-item', el => el.textContent);
+	expect(finalText).toBe('Phil (35 years old)');
+
+}, 10000)
